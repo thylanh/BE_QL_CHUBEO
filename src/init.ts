6fc199b6e63526ffea25cacf04e1b3b20cb10780
Thylanh <thylanh2523@gmail.com>
2026-09-14 19:21:43 +0700

    CORS off

--- src/init.ts
+++ src/init.ts
@@ -1,11 +1,8 @@
 import { INestApplication } from '@nestjs/common';
 
 const initApp = (app: INestApplication) => {
-  const { APP_PREFIX = '/api', FE_URL = '*' } = process.env;
+  const { APP_PREFIX = '/api' } = process.env;
   app.setGlobalPrefix(APP_PREFIX);
-  app.enableCors({
-    origin: FE_URL === '*' ? true : FE_URL.split(',').map((url) => url.trim()),
-  });
   app.enableShutdownHooks();
   return app;
 };
