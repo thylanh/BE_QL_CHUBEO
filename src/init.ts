05b6bf8e00a5f7a9de23c0062e5b420bd95c2ba8
Thylanh <thylanh2523@gmail.com>
2026-09-16 18:43:24 +0700

    fix CORS

--- src/init.ts
+++ src/init.ts
@@ -1,9 +1,28 @@
+// import { INestApplication } from '@nestjs/common';
+
+// const initApp = (app: INestApplication) => {
+//   const { APP_PREFIX = '/api' } = process.env;
+//   app.setGlobalPrefix(APP_PREFIX);
+//   app.enableShutdownHooks();
+//   return app;
+// };
+// export { initApp };
+
 import { INestApplication } from '@nestjs/common';
 
 const initApp = (app: INestApplication) => {
   const { APP_PREFIX = '/api' } = process.env;
+
   app.setGlobalPrefix(APP_PREFIX);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.enableShutdownHooks();
+
   return app;
 };
+
 export { initApp };
